884df72cd97aaf0ee406a8d0be80328018128dd4
Lucas Ribeiro <67128586+lucasrsilvak@users.noreply.github.com>
2024-06-16 11:28:43 -0300

    Update Datapath.vhd

--- aulas/lab8/Datapath/Datapath.vhd
+++ aulas/lab8/Datapath/Datapath.vhd
@@ -8,7 +8,7 @@ entity Datapath is
 		E 		: in std_logic_vector(3 downto 0);
 		Sobe	: out std_logic;
 		Desce	: out	std_logic;
-		Media	: out std_logic(6 downto 0);
+		Media	: out std_logic_vector(6 downto 0);
 		--Pinos
 		load_E 		: in std_logic;
 		Reset_MA 	: in std_logic;
@@ -17,7 +17,10 @@ entity Datapath is
 		Descendo		: in std_logic;
 		Subindo		: in std_logic;
 		Atualizar	: in std_logic;
-		clock			: in std_logic
+		clock			: in std_logic;
+		-- Desafio
+		Sig_E : buffer std_logic_vector(3 downto 0);
+		Sig_M : buffer std_logic_vector(3 downto 0)
 	);
 end Datapath;
 
@@ -27,7 +30,7 @@ architecture structural of Datapath is
 		generic
 		(
 				W: natural
-		)
+		);
 		port
 		(
 			clock: in std_logic;
@@ -36,7 +39,7 @@ architecture structural of Datapath is
 			D: 	 in std_logic_vector (W-1 downto 0);
 			Q: 	 out std_logic_vector(W-1 downto 0)
 		);
-	end component
+	end component;
 	
 	component 	Reg_MA is
 		port
@@ -46,7 +49,7 @@ architecture structural of Datapath is
 			INPUT   : in  std_logic_vector(3 downto 0);
 			OUTPUT  : out std_logic_vector(3 downto 0)
 		);
-	end component
+	end component;
 	
 	component	Comparador is
 		generic
@@ -60,7 +63,7 @@ architecture structural of Datapath is
 			a,b : in std_logic_vector(DATA_WIDTH-1 downto 0);
 			maior, menor, igual : out std_logic
 		);
-	end component
+	end component;
 	
 	component	BCD_7seg is
 		port
@@ -71,14 +74,14 @@ architecture structural of Datapath is
 	end component;
 	
 	--sinais entre os componentes
-	signal Sig_E, Sig_M : std_logic_vector(3 downto 0);
+	--signal Sig_E, Sig_M : std_logic_vector(3 downto 0);
 	
 	--inicio das instancias
 	begin
-		instancia_Reg_E		:	RegW   		generic map(W=>4) port map(clock=>clock, load=>load_E, D=>E, Q=>Sig_E);
-		instancia_Reg_SD		:	RegW   		generic map(W=>2) port map(clock=>clock, load=>Atualizar, D=>(Subindo & Descendo), Q(1)=>Sobe, Q(0)=>Desce);
+		instancia_Reg_E		:	Reg_W   		generic map(W=>4) port map(clock=>clock, reset=>'0', load=>load_E, D=>E, Q=>Sig_E);
+		instancia_Reg_SD		:	Reg_W   		generic map(W=>2) port map(clock=>clock, reset=>'0', load=>Atualizar, D(1)=>Subindo, D(0)=>Descendo, Q(1)=>Sobe, Q(0)=>Desce);
 		instancia_Reg_MA		:	Reg_MA 		port map	(clock=>clock, RESET=>Reset_MA, INPUT=>Sig_E, OUTPUT=>Sig_M);
 		instancia_Comparador	:	Comparador 	port map(a=>Sig_E,b=>Sig_M, maior=>Maior, menor=>Menor);
-		instancia_BCD_7seg	:	BCD_7seg		port map(entrada=>Sig_M, saida=>Media)
+		instancia_BCD_7seg	:	BCD_7seg		port map(entrada=>Sig_M, saida=>Media);
 	
-end structural; 
+end structural;
