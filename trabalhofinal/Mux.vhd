6e9b4494b4d5a6cdfd2a6307ff74f98209de4ef1
Lucas Ribeiro <67128586+lucasrsilvak@users.noreply.github.com>
2024-08-02 11:00:37 -0300

    Update and rename Demux.vhd to Mux.vhd

--- trabalhofinal/Mux.vhd
+++ trabalhofinal/Mux.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity Demux is
+entity Mux is
 	generic (
 		W : integer := 16;
 		Log2W : integer := 4  -- log2(16) = 4, para valores maiores, ajustar manualmente
@@ -12,9 +12,9 @@ entity Demux is
 		Selector     : in  std_logic_vector(log2W-1 downto 0);
 		Output_value : out std_logic
 	);
-end Demux;
+end Mux;
 
-architecture Behavioral of Demux is
+architecture Behavioral of Mux is
 	begin
 		process(Input_vector, Selector)
 		begin
