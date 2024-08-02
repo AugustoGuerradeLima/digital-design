4091b420914f5da8c94e2541c2249f172b77245e
André Prado Procópio <86435277+prade-p@users.noreply.github.com>
2024-08-02 00:29:12 -0300

    Update Reg_Senha.vhd

--- trabalhofinal3/Reg_Senha.vhd
+++ trabalhofinal3/Reg_Senha.vhd
@@ -4,16 +4,16 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Reg_Senha is
-    Port ( clk     : in  STD_LOGIC;
-           reset   : in  STD_LOGIC;
-           carregar: in  STD_LOGIC;
-           digito  : in  STD_LOGIC_VECTOR (3 downto 0);
-           senha   : out STD_LOGIC_VECTOR (15 downto 0));
+    Port ( clk     : in  std_logic;
+           reset   : in  std_logic;
+           carregar: in  std_logic;
+           digito  : in  std_logic_vector (3 downto 0);
+           senha   : out std_logic_vector (15 downto 0));
 end Reg_Senha;
 
 architecture Behavioral of Reg_Senha is
-    signal sig_senha : STD_LOGIC_VECTOR (15 downto 0) := (others => '0'); -- Registrador para armazenar os 4 dígitos da senha
-    signal internal_senha : STD_LOGIC_VECTOR (15 downto 0) := "0001001000110100"; -- Senha interna "1234"
+    signal sig_senha : std_logic_vector (15 downto 0) := (others => '0'); -- Registrador para armazenar os 4 dígitos da senha
+    signal last_carregar : std_logic := '0'; -- Para detectar a borda de subida de carregar
     signal count : integer := 0; -- Contador para acompanhar o número de dígitos inseridos
 begin
 
@@ -23,7 +23,7 @@ begin
         sig_senha <= (others => '0');
         count <= 0;
     elsif rising_edge(clk) then
-        if carregar = '1' then
+        if carregar = '1' and last_carregar = '0' then -- Detecta a borda de subida de carregar
             if count < 4 then
                 sig_senha(15 downto 12) <= sig_senha(11 downto 8);
                 sig_senha(11 downto 8) <= sig_senha(7 downto 4);
@@ -32,6 +32,7 @@ begin
                 count <= count + 1;
             end if;
         end if;
+        last_carregar <= carregar; -- Atualiza o último estado de carregar
     end if;
 end process;
 
